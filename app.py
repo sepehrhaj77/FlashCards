87577d12b6e753a1f11414dbbfcbb07b6d93ed28
Sepehr Hajbarat <sepehrhaj77@gmail.com>
2020-04-30 20:01:45 -0400

    Pages can now communicate with each other through passed variables

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import random
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
-    
+
+@app.route("/main", methods = ["POST"])
+def main():
+    number = request.form.get("number")
+    rando = random.randint(1,int(number))
+    return render_template("main.html", number=number, rando=rando)
+
+if __name__ == "__main__":
+    app.run(debug=True)
